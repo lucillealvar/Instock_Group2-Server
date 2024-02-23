bb307cca376f7daf69f70886a13b463614acd442
TonySMK <tonysun25light@live.ca>
2024-02-22 21:52:43 -0500

    Configured and established connection with Knex Created some test routes...

--- Server/routes/Warehouse/warehouseRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/warehouse', (req, res) => {
-    res.send('List of warehouse');
-})
-
-
-module.exports = router;
