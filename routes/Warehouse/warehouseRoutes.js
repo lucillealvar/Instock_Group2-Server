3b0ce85cfe8170c04a20777758fa7b1695011f3f
Wanessa Lopes <wanessaalopes@gmail.com>
2024-02-23 15:41:48 -0700

    Define the route to edit a warehouse. Set the validation for all fields.

--- routes/Warehouse/warehouseRoutes.js
+++ routes/Warehouse/warehouseRoutes.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const knex = require('knex');
 
 router.get('/warehouse', (req, res) => {
     res.send('List of warehouse');
 })
 
+router.put('/api/warehouses/:id', async (req, res) => {
+  const { id } = req.params;
+  const {
+    warehouse_name,
+    address,
+    city,
+    country,
+    contact_name,
+    contact_position,
+    contact_phone,
+    contact_email
+  } = req.body;
+
+  if (!warehouse_name || !address || !city || !country || !contact_name || !contact_position || !contact_phone || !contact_email) {
+    return res.status(400).json({ message: 'All fields are required' });
+  } 
+
+  const phoneRegex = /^(\([0-9]{3}\)\s*|[0-9]{3}\-)[0-9]{3}-[0-9]{4}$/;
+  const emailRegex = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
 
 module.exports = router;
