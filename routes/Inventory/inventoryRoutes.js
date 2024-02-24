5bd8286c71e59918da0da1d3dc7ef03ab25c1b9f
TonySMK <tonysun25light@live.ca>
2024-02-24 01:49:56 -0500

    init get-single-inventory-item branch

--- routes/Inventory/inventoryRoutes.js
+++ routes/Inventory/inventoryRoutes.js
@@ -3,23 +3,23 @@ const router = express.Router();
 
 const knex = require("knex")(require("../../knexfile"));
 
+// router.get("/", (req, res) => {
+//   res.send("@ inventory default route");
+// });
+
+// router.get("api/simplyall", (req, res) => {
+//   // returns all Inventory objects, unfiltered
+//   knex("inventories")
+//     .select("*")
+//     .then((data) => {
+//       res.status(200).json(data);
+//     })
+//     .catch((error) => {
+//       res.status(500).send("na-da");
+//     });
+// });
+
 router.get("/", (req, res) => {
-  res.send("@ inventory default route");
-});
-
-router.get("/simplyall", (req, res) => {
-  // returns all Inventory objects, unfiltered
-  knex("inventories")
-    .select("*")
-    .then((data) => {
-      res.status(200).json(data);
-    })
-    .catch((error) => {
-      res.status(500).send("na-da");
-    });
-});
-
-router.get("/withwarehousenames", (req, res) => {
   // return all Inventory object, replace inventory.warehouse_id
   // with warehouses.warehouse_name
   knex
@@ -40,6 +40,12 @@ router.get("/withwarehousenames", (req, res) => {
     .catch((error) => {
       res.status(500).send("na-da");
     });
+});
+
+router.get("/:intventoyid", (req, res) => {
+  let selectid = req.params.intventoyid;
+  console.log(selectid);
+  res.send(selectid);
 });
 
 module.exports = router;
