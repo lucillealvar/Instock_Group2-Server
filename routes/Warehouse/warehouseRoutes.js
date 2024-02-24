3ed146071f6edc634d0f357b849a0e26230bdc92
Lucille Alvar <lucille.alvar@gmail.com>
2024-02-24 00:38:51 -0700

    Created endpoint to send GET request for a filtered list of warehouses. Updated middleware routes from server.js On branch feature/JL23P2-14/GET-list-all-warehouses Changes to be committed: modified: routes/Warehouse/warehouseRoutes.js modified: server.js On branch feature/JL23P2-14/GET-list-all-warehouses Your branch is up to date with 'origin/feature/JL23P2-14/GET-list-all-warehouses'.
    Changes to be committed:
    	modified:   routes/Warehouse/warehouseRoutes.js

--- routes/Warehouse/warehouseRoutes.js
+++ routes/Warehouse/warehouseRoutes.js
@@ -5,13 +5,25 @@ const knex = require("knex")(require("../../knexfile"));
 router.get("/", (req, res) => {
   // fetch all warehouses from the database
   knex("warehouses")
-    .select("*")
+    .select(
+      "id",
+      "warehouse_name",
+      "address",
+      "city",
+      "country",
+      "contact_name",
+      "contact_position",
+      "contact_phone",
+      "contact_email"
+    )
     .then((warehouses) => {
       res.status(200).json(warehouses);
     })
     .catch((error) => {
       console.error(error);
-      res.status(500).json({ error: "Something went wrong. Please try again later" });
+      res
+        .status(500)
+        .json({ error: "Something went wrong. Please try again later" });
     });
 });
 
